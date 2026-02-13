722df8e83f07503f45a2e4b328dbad45ce31ec13
Thorsten Altenkirch <psztxa@exmail.nottingham.ac.uk>
2026-02-13 12:32:32 +0000

    changed lecture import

--- Lectures.lean
+++ Lectures.lean
@@ -1,3 +1,6 @@
--- import all lectures so far
-import Lectures.L02
-import Lectures.L03
+-- import most recent lecture
+-- import Lectures.L02
+-- import Lectures.L03
+-- import Lectures.L04
+-- import Lectures.L05
+import Lectures.L06
